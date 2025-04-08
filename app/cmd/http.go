0fb66d87306ad647c33dfa09bfa541f408bc203c
yikakia <yika_w@outlook.com>
2025-04-08 09:52:52 +0800

    变量绑定

--- app/cmd/http.go
+++ app/cmd/http.go
@@ -5,32 +5,32 @@ import (
 	"github.com/yikakia/nga_grep/handler"
 )
 
+var (
+	port            string
+	corsAllowOrigin []string
+	rDB             string
+)
+
 func init() {
-	runHttpServerCmd.PersistentFlags().StringP("port", "p", ":11648", "Port to listen on")
-	runHttpServerCmd.PersistentFlags().StringSlice("cors", []string{"localhost"}, "CORS Allow Origin 的域名, 逗号分割")
-	runHttpServerCmd.PersistentFlags().String("db", "./nga.db", "db 路径")
+	runHttpServerCmd.PersistentFlags().StringVarP(&port, "port", "p", ":11648", "Port to listen on")
+	runHttpServerCmd.PersistentFlags().StringSliceVar(&corsAllowOrigin, "cors", []string{"localhost"}, "CORS Allow Origin 的域名, 逗号分割")
+	runHttpServerCmd.PersistentFlags().StringVar(&rDB, "db", "", "db 路径")
 }
 
 var runHttpServerCmd = &cobra.Command{
 	Use:   "api-server",
 	Short: "启动http服务, 监听指定端口",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		err := cmd.MarkFlagRequired("db")
+		if err != nil {
+			panic(err)
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		port, err := cmd.PersistentFlags().GetString("port")
-		if err != nil {
-			panic(err)
-		}
-		corsAllowOrigin, err := cmd.PersistentFlags().GetStringSlice("cors")
-		if err != nil {
-			panic(err)
-		}
-		db, err := cmd.PersistentFlags().GetString("db")
-		if err != nil {
-			panic(err)
-		}
 		handler.RunHttpServer(handler.RunHttpServerConfig{
 			Port:            port,
 			CorsAllowOrigin: corsAllowOrigin,
-			DB:              db,
+			DB:              rDB,
 		})
 	},
 }
--- app/cmd/sync.go
+++ app/cmd/sync.go
@@ -5,34 +5,24 @@ import (
 	"github.com/yikakia/nga_grep/handler"
 )
 
+var (
+	cid string
+	uid string
+	url string
+	db  string
+)
+
 func init() {
-	syncCmd.PersistentFlags().String("cid", "cid", "cookie 中的 ngaPassportCid")
-	syncCmd.PersistentFlags().String("uid", "uid", "cookie 中的 ngaPassportUid")
-	syncCmd.PersistentFlags().String("url", "https://bbs.nga.cn", "nga域名")
-	syncCmd.PersistentFlags().String("db", "./nga.db", "db 路径")
+	syncCmd.Flags().StringVar(&cid, "cid", "cid", "cookie 中的 ngaPassportCid")
+	syncCmd.Flags().StringVar(&uid, "uid", "uid", "cookie 中的 ngaPassportUid")
+	syncCmd.Flags().StringVar(&url, "url", "https://bbs.nga.cn", "nga域名")
+	syncCmd.Flags().StringVar(&db, "db", "./nga.db", "db 路径")
 }
 
 var syncCmd = &cobra.Command{
 	Use:   "sync",
 	Short: "爬取帖子",
 	Run: func(cmd *cobra.Command, args []string) {
-
-		cid, err := cmd.PersistentFlags().GetString("cid")
-		if err != nil {
-			panic(err)
-		}
-		uid, err := cmd.PersistentFlags().GetString("uid")
-		if err != nil {
-			panic(err)
-		}
-		url, err := cmd.PersistentFlags().GetString("url")
-		if err != nil {
-			panic(err)
-		}
-		db, err := cmd.PersistentFlags().GetString("db")
-		if err != nil {
-			panic(err)
-		}
 		handler.SyncServer(handler.SyncServerConfig{
 			Cid: cid,
 			Uid: uid,
