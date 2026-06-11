d0c632057f03aa8c174332a3f4fcba5f95e283ed
yikakia <yika_w@outlook.com>
2026-06-11 15:00:44 +0800

    增加埋点

--- handler/http/ratelimit.go
+++ handler/http/ratelimit.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"log/slog"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/yikakia/nga_grep/internal/ratelimit"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
 )
+
+var tracer = sync.OnceValue(func() trace.Tracer {
+	return otel.Tracer("")
+})
 
 func isAllow(c *gin.Context, start, end time.Time, duration time.Duration) (isAllow bool) {
 	ctx := c.Request.Context()
 	defer func() { recordRateLimit(ctx, isAllow) }()
+	ctx, sp := tracer().Start(ctx, "ratelimit")
+	defer sp.End()
 
 	if duration <= 0 {
 		slog.WarnContext(ctx, fmt.Sprintf("duration should > 0 but got %v", duration))
